3cb2825986014f54b11610884ff6514e0f7ffb7d
Mubeen Ahmed <mubeen.ahmed@skillorbit.com>
2018-12-04 16:47:10 +0500

    adding slick and trying mapping to domain

--- src/main/scala/infrastructure/slick/SlickCategoryRepository.scala
+++ src/main/scala/infrastructure/slick/SlickCategoryRepository.scala
@@ -5,6 +5,7 @@ import domain.product.entities.Category
 import domain.product.repositories.CategoryRepository
 import domain.product.valueobjects.{CategoryId, ProductId}
 import domain.product.entities.Product
+import infrastructure.slick.tables.{SlickCategory, SlickProduct}
 import slick.jdbc.GetResult
 import slick.jdbc.H2Profile.api._
 
@@ -13,25 +14,16 @@ import scala.concurrent.Future
 class SlickCategoryRepository extends CategoryRepository with SlickRepository
 {
 
-  val getProductResult = GetResult(r => Product(r.<<, r.<<, r.<<, r.<<))
-  val getCategoryResult = GetResult(r => Category(r.<<, r.<<, r.<<, r.<<))
+  val product = TableQuery[SlickProduct]
+  val category = TableQuery[SlickCategory]
 
-  override def findOneById(categoryId: CategoryId): Future[Category] =
-    db.run(sql"select * from category where id = ${categoryId}".as[Category](getCategoryResult))
-      .map(c => c.head)
+  override def findOneById(categoryId: CategoryId): Future[Category] = ???
 
+  override def findAll: Future[List[Category]] = ???
 
-  override def findAll: Future[List[Category]] =
-    db.run(sql"select * from category".as[Category](getCategoryResult))
-      .map(c => c.toList)
+  override def findAllProductsByCategoryId(categoryId: CategoryId): Future[List[Product]] = ???
 
-  override def findAllProductsByCategoryId(categoryId: CategoryId): Future[List[Product]] =
-    db.run(sql"select * from product where categoryId = ${categoryId}".as[Product](getProductResult))
-      .map(c => c.toList)
-
-  override def findOneProductById(productId: ProductId): Future[Product] =
-    db.run(sql"select * from product where productId = ${productId}".as[Product](getProductResult))
-      .map(c => c.head)
+  override def findOneProductById(productId: ProductId): Future[Product] = ???
 
   override def save(category: Category): Future[Category] = ???
 
--- src/main/scala/infrastructure/slick/tables/Product.scala
+++
@@ -1,9 +0,0 @@
-package infrastructure.slick.tables
-
-import slick.lifted.Tag
-import slick.model.Table
-
-class Product(tag: Tag) extends Table[(Int, String,Double, Double, Int)](tag, "product")
-{
-  def id = columns[Int]("id", O.PrimaryKey)
-}
